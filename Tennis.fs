40162fd5f24b44946bf225597ac7bcd49c57c8e0
Jean-Baptiste Dusseaut <157962+BodySplash@users.noreply.github.com>
2021-06-04 14:57:33 +0200

    refactor: make unvalid scores impossible to create

--- Tennis.fs
+++ Tennis.fs
@@ -10,18 +10,20 @@ namespace Tennis
 
 *)
 
+
 type Player =
     | A
     | B
+
 
 type PlayerPoints =
     | Zero
     | Fifteen
     | Thirty
-    | Forty
 
 type Score =
     | Points of PlayerPoints * PlayerPoints
+    | Forty of Player * PlayerPoints
     | Advantage of Player
     | Deuce
     | Game of Player
@@ -30,29 +32,58 @@ type Score =
 module Game =
 
 
-    let private nextPoint =
+    let private incrementPoint =
         function
-        | Zero -> Fifteen
-        | Fifteen -> Thirty
-        | Thirty -> Forty
-        | _ -> failwith "Laul what"
+        | Zero -> Some Fifteen
+        | Fifteen -> Some Thirty
+        | Thirty -> None
 
-    let normalizeScore =
+    let private pointsOf playerAPoints playerBPoints player =
+        match player with
+        | A -> playerAPoints
+        | B -> playerBPoints
+
+    let private pointTo player point playerAPoints playerBPoints =
+        match player with
+        | A -> Points(point, playerBPoints)
+        | B -> Points(playerAPoints, point)
+
+    let private otherPlayer =
         function
-        | Points (Forty, Forty) -> Deuce
-        | s -> s
+        | A -> B
+        | B -> A
+
+    let scoreWithPoints winner playerAPoints playerBPoints =
+        let currentPointOf = pointsOf playerAPoints playerBPoints
+
+        match currentPointOf winner |> incrementPoint with
+        | Some v -> pointTo winner v playerAPoints playerBPoints
+        | None -> Forty(winner, (currentPointOf (otherPlayer winner)))
+
+    let scoreWithForty winner player otherPlayer =
+        if player = winner then
+            Game winner
+        else
+            match incrementPoint otherPlayer with
+            | Some v -> Forty(player, v)
+            | None -> Deuce
+
+    let scoreWithAdvantage winner player =
+        if winner = player then
+            Game winner
+        else
+            Deuce
+
+    let scoreWithDeuce winner = Advantage winner
 
     let score points =
         points
         |> Seq.fold
             (fun score player ->
                 match score, player with
-                | Advantage p1, p2 when p1 = p2 -> Game p1
-                | Advantage _, _ -> Deuce
-                | Deuce, p -> Advantage p
-                | Points (Forty, _), A -> Game A
-                | Points (_, Forty), B -> Game B
-                | Points (a, b), A -> Points(nextPoint a, b) |> normalizeScore
-                | Points (a, b), B -> Points(a, nextPoint b) |> normalizeScore
+                | Advantage player, winner -> scoreWithAdvantage winner player
+                | Deuce, winner -> scoreWithDeuce winner
+                | Forty (player, other), winner -> scoreWithForty winner player other
+                | Points (a, b), winner -> scoreWithPoints winner a b
                 | _, _ -> score)
             (Points(Zero, Zero))
